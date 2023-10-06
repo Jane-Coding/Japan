90bc56ac3414b9a4a189ee6cc8b3f068cb463e06
Jane Doe <raidho27@gmail.com>
2023-10-06 12:43:17 +0300

    add texts, variables, js for txt-change

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ const upButton = document.getElementById("up-button");
 const downButton = document.getElementById("down-button");
 const rotation = document.getElementById("rotation");
 
-const center = document.getElementById("center");
+const textChange = document.getElementById("text-change");
+
+const emoji = document.getElementById("emoji");
+const sakura = document.getElementById("sakura");
+const origami = document.getElementById("origami");
+const sudoku = document.getElementById("sudoku");
+const words = [emoji, sudoku, origami, sakura];
 
 accordion.addEventListener("click", function (e) {
   if (!e.target.classList.contains("accordion")) {
@@ -17,30 +23,44 @@ accordion.addEventListener("click", function (e) {
     }
     if (!e.target.classList.contains("show")) {
       e.target.classList.add("show");
-      descriptionHeader.classList.toggle("fadeIn");
       if (e.target.classList.contains("fuji")) {
-        descriptionHeader.innerHTML = "Discover picturesque scenery";
+        descriptionText.innerHTML = "Discover picturesque scenery";
       } else if (e.target.classList.contains("garden")) {
-        descriptionHeader.innerHTML = "Relax in peaceful gardens";
+        descriptionText.innerHTML = "Relax in peaceful gardens";
       } else if (e.target.classList.contains("street")) {
-        descriptionHeader.innerHTML = "Walk urban streets";
+        descriptionText.innerHTML = "Walk urban streets";
       } else if (e.target.classList.contains("tori")) {
-        descriptionHeader.innerHTML = "Visit shinto shrines";
+        descriptionText.innerHTML = "Visit shinto shrines";
       }
     }
   }
 });
 
 let degrees = 0;
+let i = 0;
 
 upButton.addEventListener("click", function () {
   degrees += 90;
   let clockwise = `rotate(${degrees}deg)`;
   rotation.style.transform = clockwise;
+
+  words[i].classList.toggle("invisible");
+  ++i;
+  if (i >= 4) {
+    i = 0;
+  }
+  words[i].classList.toggle("invisible");
 });
 
 downButton.addEventListener("click", function () {
   degrees -= 90;
   let counterclockwise = `rotate(${degrees}deg)`;
   rotation.style.transform = counterclockwise;
+
+  words[i].classList.toggle("invisible");
+  --i;
+  if (i <= -1) {
+    i = 3;
+  }
+  words[i].classList.toggle("invisible");
 });
